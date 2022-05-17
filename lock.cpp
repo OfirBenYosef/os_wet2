db7ee27daeaf7331d70d1ec5df1d8fdf2beadc7b
ofirby <ofben-yosef@campus.technion.ac.il>
2022-05-17 13:31:24 +0300

    with lock

--- lock.cpp
+++ lock.cpp
@@ -6,16 +6,17 @@
 //
 
 #include "lock.h"
+using namespace std;
 /**************************************************************************************************/
 
 locker::locker()
 {
     readers_counter = 0;
-    if (pthread_mutex_init(&read, nullptr) != 0) {
+    if(pthread_mutex_init(&read, nullptr) != 0) {
         perror("pthread_mutex_init has failed");
         exit(1);
     }
-    if (pthread_mutex_init(&write, nullptr) != 0) {
+    if(pthread_mutex_init(&write, nullptr) != 0) {
         perror("pthread_mutex_init has failed");
         exit(1);
     }
@@ -23,19 +24,62 @@ locker::locker()
 
 locker::~locker()
 {
-    if (pthread_mutex_destroy(&read) != 0) {
+    if(pthread_mutex_destroy(&read) != 0) {
         perror("pthread_mutex_destroy has failed");
         exit(1);
     }
-    if (pthread_mutex_destroy(&write) != 0) {
+    if(pthread_mutex_destroy(&write) != 0) {
         perror("pthread_mutex_destroy has failed");
         exit(1);
     }
 }
-void locker::add_reader(){}
-void locker::remove_reader(){}
-void locker::add_writer(){}
-void locker::remove_writer(){}
+void locker::add_reader(){
+    if(pthread_mutex_lock(&read) != 0) {
+        perror("pthread_mutex_lock has failed");
+        exit(1);
+    }
+    readers_counter++;
+    if(readers_counter == 1) {
+        if(pthread_mutex_lock(&write) != 0) {
+            perror("pthread_mutex_lock has failed");
+            exit(1);
+        }
+    }
+    if(pthread_mutex_unlock(&read) != 0) {
+        perror("pthread_mutex_unlock has failed");
+        exit(1);
+    }
+}
+void locker::remove_reader(){
+    if(pthread_mutex_lock(&read) != 0) {
+        perror("pthread_mutex_lock has failed");
+        exit(1);
+    }
+    readers_counter--;
+    if (readers_counter == 0) {
+        if (pthread_mutex_unlock(&write) != 0) {
+            perror("pthread_mutex_unlock has failed");
+            exit(1);
+        }
+    }
+    if (pthread_mutex_unlock(&read) != 0) {
+        perror("pthread_mutex_unlock has failed");
+        exit(1);
+    }
+}
+void locker::add_writer(){
+    if (pthread_mutex_lock(&write) != 0) {
+        perror("pthread_mutex_lock has failed");
+        exit(1);
+    }
+}
+
+void locker::remove_writer(){
+    if (pthread_mutex_unlock(&write) != 0) {
+        perror("pthread_mutex_unlock has failed");
+        exit(1);
+    }
+}
 /**************************************************************************************************/
 log::log()
 {
